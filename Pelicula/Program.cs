3c043b81dbe049074a8a11b105536ec2862f9cff
Marlonc137 <60365571+Marlonc137@users.noreply.github.com>
2020-02-20 23:58:58 -0800

    atributos públicos

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -14,7 +14,15 @@ class Program
     {
         static void Main(string[] args)
         {
+          Pelicula Numero1 = new Pelicula();
+          Numero1.titulo="El irlandés";
+          Numero1.año=2019;
+          Numero1.pais="Estados Unidos";
+          Numero1.director="Martin Scorsese";
 
+          Console.WriteLine(" Pelicula: {0}, Año: {1}, Pais: {2}, Director: {3}",Numero1.titulo,Numero1.año,Numero1.pais,Numero1.director);
+           
+           
         }
 
         
