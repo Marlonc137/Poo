13a07e852cd6b36b87fa4ebab0c3dcc71f9952f6
Marlonc137 <60365571+Marlonc137@users.noreply.github.com>
2020-02-26 06:14:25 -0800

    Lista peliculas

---
+++ Lista/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Lista
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<String> Lista = new List<String>();
+            Lista.add("El Rey Leon");
+            Lista.add("Ipman");
+            Lista.add("Bad Boys");
+            Lista.add("Joker");
+            Lista.add("Sonic");
+
+            foreach(string Pelicula in Lista)
+            Console.WriteLine(Pelicula);
+
+
+        }
+    }
+}
