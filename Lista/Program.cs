3401c2a6566888430cfedf13012c34b8c79cf88e
Marlonc137 <60365571+Marlonc137@users.noreply.github.com>
2020-02-26 20:04:13 -0800

    FinalLista

--- Lista/Program.cs
+++ Lista/Program.cs
@@ -2,19 +2,35 @@
 using System.Collections.Generic;
 namespace Lista
 {
+  class peliculas
+  {
+      public string titulo;
+      public void final()
+      {
+              Console.WriteLine("({0})",titulo);
+      }
+
+      public peliculas(string t) 
+      {
+          titulo=t;
+      }
+  }
+
+
+
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            List<String> Lista = new List<String>();
-            Lista.add("El Rey Leon");
-            Lista.add("Ipman");
-            Lista.add("Bad Boys");
-            Lista.add("Joker");
-            Lista.add("Sonic");
+            List<peliculas> Lista = new List<peliculas>();
+            Lista.Add(new peliculas("El Rey Leon"));
+            Lista.Add(new peliculas("Ipman"));
+            Lista.Add(new peliculas("Bad Boys"));
+            Lista.Add(new peliculas("Joker"));
+            Lista.Add(new peliculas("Sonic"));
 
-            foreach(string Pelicula in Lista)
-            Console.WriteLine(Pelicula);
+            foreach(peliculas t in Lista)
+            t.final();
 
 
         }
