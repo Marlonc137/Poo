5b35fc5c2a5b6c953dd28ed89e644e244d715ffe
Marlonc137 <60365571+Marlonc137@users.noreply.github.com>
2020-03-06 12:51:04 -0800

    publico

---
+++ Actores/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Actores
+{
+
+class peliculas
+{
+  public string titulo;
+  public Int16 año;
+  public string pais;
+  public string director;
+
+}
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            peliculas p1= new peliculas();
+
+            p1.titulo=("el joker");
+            p1.año=(2019);
+            p1.pais=("Estados unidos");
+            p1.director=("Todd Phillips");
+
+            Console.WriteLine("La pelicula es: "+p1.titulo);
+            Console.WriteLine("El año es: "+ p1.año);
+            Console.WriteLine("El pais es:"+p1.pais);
+            Console.WriteLine("El director es: " +p1.director);
+
+        }
+    }
+}
