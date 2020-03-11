725ebdb3fce9b2c69a4c8649c996a48ac2031de7
Marlonc137 <60365571+Marlonc137@users.noreply.github.com>
2020-03-10 17:15:24 -0700

    2

--- Actores/Program.cs
+++ Actores/Program.cs
@@ -5,12 +5,71 @@ namespace Actores
 
 class peliculas
 {
-  public string titulo;
-  public Int16 año;
-  public string pais;
-  public string director;
+  private string titulo;
+  private Int16 año;
+  private string pais;
+  private string Director;
+
+
+  public void settitulo(string titulo)
+{
+    this.titulo= titulo;
+}
+
+public string gettitulo()
+{
+    return this.titulo;
+}
+
+  public void setaño(Int16 año)
+    {
+      this.año= año;
+    }
+    
+    public Int16 getaño()
+    {
+      return this.año;
+    }
+
+public void setpais(string pais)
+{
+  this.pais=pais;
+}
+
+public string getpais()
+{
+  return this.pais;
+}
+
+public void setdirector(string Director)
+{
+  this.Director=Director;
+}
+ 
+public string getdirector()
+{
+  return this.Director;
+}
+
+public peliculas(string titulo, Int16 año, string pais, string Director)//constructor 1
+{
+ this.titulo=titulo;
+ this.año=año;
+ this.pais=pais;
+ this.Director=Director;
 
 }
+ public peliculas()// constructor 2
+ {
+
+ }
+
+
+
+  }
+
+
+
     class Program
     {
 
@@ -19,7 +78,7 @@ class peliculas
         {
             peliculas p1= new peliculas();
 
-            p1.titulo=("el joker");
+            /*p1.titulo=("el joker");
             p1.año=(2019);
             p1.pais=("Estados unidos");
             p1.director=("Todd Phillips");
@@ -27,7 +86,25 @@ class peliculas
             Console.WriteLine("La pelicula es: "+p1.titulo);
             Console.WriteLine("El año es: "+ p1.año);
             Console.WriteLine("El pais es:"+p1.pais);
-            Console.WriteLine("El director es: " +p1.director);
+            Console.WriteLine("El director es: " +p1.director);*/
+
+            p1.settitulo("El irlandes");
+           p1.setaño(2019);
+           p1.setpais("Estados Unidos");
+           p1.setdirector("Martin Scorsese");
+           Console.WriteLine(" Pelicula 1: {0}, Año: {1}, Pais: {2}, Director: {3}",p1.gettitulo(),p1.getaño(),p1.getpais(),p1.getdirector());
+
+           peliculas p2= new peliculas();//// Es para declarar un nueva pelicula
+           p2.settitulo("Joker de Joaquin Phoenix");
+           p2.setaño(2019);
+           p2.setpais("Estados Unidos");
+           p2.setdirector("Todd Phillips");
+           Console.WriteLine(" Pelicula 1: {0}, Año: {1}, Pais: {2}, Director: {3}",p2.gettitulo(),p2.getaño(),p2.getpais(),p2.getdirector());
+
+
+
+
+
 
         }
     }
