3babdc46bc7180828ba0a50c4716251e64957459
Marlonc137 <60365571+Marlonc137@users.noreply.github.com>
2020-03-10 17:28:15 -0700

    Actores

--- Actores/Program.cs
+++ Actores/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Actores
 {
@@ -9,6 +10,22 @@ class peliculas
   private Int16 año;
   private string pais;
   private string Director;
+
+
+private List<Actor> actores = new List<Actor>();
+
+        public void AgregarActor(Actor actor)
+        {
+            actores.Add(actor);
+        }
+
+        public void ImprimeActor()
+        {
+            foreach (Actor Actor in actores)
+            {
+                Console.WriteLine("{0}({1})", Actor.Nombre, Actor.Año);
+            }
+        }
 
 
   public void settitulo(string titulo)
@@ -68,7 +85,18 @@ public peliculas(string titulo, Int16 año, string pais, string Director)//const
 
   }
 
+ class Actor
+    {
+        public string Nombre;
+        public Int16 Año;
 
+        public Actor(string nombre, Int16 año)
+        {
+           this.Nombre = nombre;
+           this.Año = año;
+        }
+
+    }
 
     class Program
     {
@@ -92,15 +120,21 @@ public peliculas(string titulo, Int16 año, string pais, string Director)//const
            p1.setaño(2019);
            p1.setpais("Estados Unidos");
            p1.setdirector("Martin Scorsese");
+           p1.AgregarActor(new Actor("Joaquin Phoenix", 2019));
+            p1.AgregarActor(new Actor("Robert De Niro", 2019));
            Console.WriteLine(" Pelicula 1: {0}, Año: {1}, Pais: {2}, Director: {3}",p1.gettitulo(),p1.getaño(),p1.getpais(),p1.getdirector());
+            p1.ImprimeActor();
+
 
            peliculas p2= new peliculas();//// Es para declarar un nueva pelicula
            p2.settitulo("Joker de Joaquin Phoenix");
            p2.setaño(2019);
            p2.setpais("Estados Unidos");
            p2.setdirector("Todd Phillips");
+           p2.AgregarActor(new Actor("Al Pacino", 2019));
+           p2.AgregarActor(new Actor("Robert De Niro", 2019));
            Console.WriteLine(" Pelicula 1: {0}, Año: {1}, Pais: {2}, Director: {3}",p2.gettitulo(),p2.getaño(),p2.getpais(),p2.getdirector());
-
+           p2.ImprimeActor();
 
 
 
